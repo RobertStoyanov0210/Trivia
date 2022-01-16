a09cca37947d827ce617aa947dd2afaee32be620
Robert Stoyanov <robert@hlebarov.com>
2022-01-16 16:12:46 +0200

    Getting information from API

---
+++ app/src/main/java/com/example/trivia/data/QuestionBank.java
@@ -0,0 +1,40 @@
+package com.example.trivia.data;
+
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.trivia.controller.AppController;
+import com.example.trivia.model.Question;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionBank {
+    ArrayList<Question> questionArrayList = new ArrayList<>();
+    private String url = "https://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements-data.json";
+
+    public List<Question> getQuestions() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, (JSONArray) null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("JSON", "onResponse: " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        AppController.getInstance().addToRequestQueue(jsonArrayRequest);
+        return null;
+
+    }
+
+}
