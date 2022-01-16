32efef155a1b726f34f2c8130e67394aa9a81ead
Robert Stoyanov <robert@hlebarov.com>
2022-01-16 16:00:06 +0200

    Model: Question added

---
+++ app/src/main/java/com/example/trivia/model/Question.java
@@ -0,0 +1,31 @@
+package com.example.trivia.model;
+
+public class Question {
+    private String answer;
+    private boolean answerTrue;
+
+    public Question() {
+
+    }
+
+    public Question(String answer, boolean answerTrue) {
+        this.answer = answer;
+        this.answerTrue = answerTrue;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public boolean isAnswerTrue() {
+        return answerTrue;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        this.answerTrue = answerTrue;
+    }
+}
